e27800b4b3f2aa9bfa50ca30c0d9f764535fb416
ishansavaliya <ishansavaliya787@gmail.com>
2025-04-19 19:51:57 +0530

    Add Styling for ToDo

--- TodoApp/app/index.tsx
+++ TodoApp/app/index.tsx
@@ -1,5 +1,22 @@
-import { FlatList, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons } from "@expo/vector-icons";
+import { Checkbox } from "expo-checkbox";
+
+type ToDoType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
 
 export default function Index() {
   const todoData = [
@@ -21,7 +38,7 @@ export default function Index() {
     {
       id: 4,
       title: "Todo 4",
-      isDone: false,
+      isDone: true,
     },
     {
       id: 5,
@@ -30,22 +47,143 @@ export default function Index() {
     },
   ];
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => {}}>
+          <Ionicons name="menu" size={24} color="#333" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {}}>
+          <Image
+            source={{
+              uri: "https://img.icons8.com/fluent/600/microsoft-todo-2019.png",
+            }}
+            style={{ width: 35, height: 35, borderRadius: 20 }}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.searchBar}>
+        <Ionicons name="search" size={24} color="#333" />
+        <TextInput
+          placeholder="Search"
+          style={styles.searchInput}
+          clearButtonMode="always"
+        />
+      </View>
+
       <FlatList
         data={todoData}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View>
-            <Text>{item.title}</Text>
-          </View>
-        )}
+        renderItem={({ item }) => <ToDoItem todo={item} />}
       />
+
+      <KeyboardAvoidingView
+        style={styles.footer}
+        behavior="padding"
+        keyboardVerticalOffset={10}
+      >
+        <TextInput placeholder="Add new ToDo" style={styles.newTodoInput} />
+        <TouchableOpacity style={styles.addButton} onPress={() => {}}>
+          <Ionicons name="add" size={34} color="#fff" />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+const ToDoItem = ({ todo }: { todo: ToDoType }) => (
+  <View style={styles.todoContainer}>
+    <View style={styles.todoInfoContainer}>
+      <Checkbox
+        value={todo.isDone}
+        color={todo.isDone ? "#3599ea" : undefined}
+        style={{ borderRadius: 2 }}
+      />
+      <Text
+        style={[
+          styles.todoText,
+          todo.isDone && { textDecorationLine: "line-through" },
+        ]}
+      >
+        {todo.title}
+      </Text>
+    </View>
+    <TouchableOpacity
+      onPress={() => {
+        alert("Deleted " + todo.id);
+      }}
+    >
+      <Ionicons name="trash" size={24} color="red" />
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+    backgroundColor: "f5f5f5",
+  },
+
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 10,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+
+  searchBar: {
+    backgroundColor: "#fff",
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 10,
+    gap: 10,
+    marginBottom: 20,
+  },
+
+  searchInput: {
+    flex: 1,
+    fontSize: 16,
+    color: "#333",
+  },
+  todoContainer: {
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  todoInfoContainer: {
+    flexDirection: "row",
+    gap: 10,
+    alignItems: "center",
+  },
+
+  todoText: {
+    fontSize: 16,
+    color: "#333",
+  },
+
+  footer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  newTodoInput: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+    borderRadius: 10,
+    fontSize: 16,
+    color: "#333",
+  },
+  addButton: {
+    backgroundColor: "#3599ea",
+    padding: 8,
+    borderRadius: 10,
+    marginLeft: 20,
+  },
+});
