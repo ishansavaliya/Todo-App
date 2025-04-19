77b121f422be1ecb3ec33152d45b01eeac31497f
ishansavaliya <ishansavaliya787@gmail.com>
2025-04-19 18:02:38 +0530

    Rendering Demo Todos

--- TodoApp/app/index.tsx
+++ TodoApp/app/index.tsx
@@ -1,15 +1,51 @@
-import { Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const todoData = [
+    {
+      id: 1,
+      title: "Todo 1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "Todo 2",
+      isDone: false,
+    },
+    {
+      id: 3,
+      title: "Todo 3",
+      isDone: false,
+    },
+    {
+      id: 4,
+      title: "Todo 4",
+      isDone: false,
+    },
+    {
+      id: 5,
+      title: "Todo 5",
+      isDone: false,
+    },
+  ];
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <FlatList
+        data={todoData}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
